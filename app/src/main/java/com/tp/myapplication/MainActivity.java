7aceb3e4472db049747404f9b22115d52f68ede8
yoann2968 <yoann.spriet@gmail.com>
2018-10-09 18:13:22 +0200

    TP_2 finit

--- app/src/main/java/com/tp/myapplication/MainActivity.java
+++ app/src/main/java/com/tp/myapplication/MainActivity.java
@@ -1,13 +1,150 @@
 package com.tp.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.graphics.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Vibrator;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener, SensorEventListener {
+
+    private float x;
+    private float y;
+
+    final String TAG = "sensor";
+
+    private LinearLayout canvasLayout = null;
+    MySurfaceView customSurfaceView = null;
+
+    SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    private Camera camera;
+
+    private Boolean isPreview;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorDetection();
+
+        canvasLayout = (LinearLayout) findViewById(R.id.customViewLayout);
+
+        // Make app full screen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        customSurfaceView = new MySurfaceView(this);
+        customSurfaceView.setOnTouchListener(this);
+        canvasLayout.addView(customSurfaceView);
+
+        if (mSensorManager == null) {
+            mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        }
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        /*isPreview = false;
+        camera = Camera.open();*/
     }
+
+    /* If user finger touch the surfaceview object. */
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        // If user touch the custom SurfaceView object.
+        if (view instanceof SurfaceView) {
+
+            // Create and set a red paint to custom surfaceview.
+            x = motionEvent.getX();
+            y = motionEvent.getY();
+            customSurfaceView.drawRedBall(x, y);
+
+            // Tell android os the onTouch event has been processed.
+            return true;
+        } else {
+            // Tell android os the onTouch event has not been processed.
+            return false;
+        }
+    }
+
+    void sensorDetection() {
+        SensorManager mSensorManager;
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        assert mSensorManager != null;
+        List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        if (deviceSensors != null && !deviceSensors.isEmpty()) {
+            for (Sensor mySensor : deviceSensors) {
+                Log.v(TAG, "info : " + mySensor.toString());
+            }
+            if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+                Log.v(TAG, "info:Accelerometer_found_!");
+            } else {
+                Log.v(TAG, "info:Accelerometer_not_found");
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mSensorManager != null) {
+            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mSensorManager != null) {
+            mSensorManager.unregisterListener(this);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        // Many sensors return 3 values , one for each axis .
+        float Ax = sensorEvent.values[0];
+        float Ay = sensorEvent.values[1];
+        float Az = sensorEvent.values[2];
+
+        if (x != 0 && y != 0) {
+            // Create and set a red paint to custom surfaceview.
+            x -= Ax;
+            y += Ay;
+            customSurfaceView.drawRedBall(x, y);
+        }
+
+        if (x + 100 >= customSurfaceView.getWidth() | y <= 0 | x <= 0 | y + 100 >= customSurfaceView.getHeight()) {
+            Vibrator vib;
+            vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            vib.vibrate(100);
+        }
+
+
+        // Do something with this sensor value .
+        Log.v(TAG, " TimeAcc = " + sensorEvent.timestamp + " Ax = " + Ax + " " + " Ay = " + Ay + " " + " Az = " + Az);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
 }
+
+
---
+++ app/src/main/java/com/tp/myapplication/MySurfaceView.java
@@ -0,0 +1,80 @@
+package com.tp.myapplication;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+
+/**
+ * Created by yoann on 09/10/2018.
+ */
+
+public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private SurfaceHolder surfaceHolder = null;
+
+    public MySurfaceView(Context context) {
+        super(context);
+
+        setFocusable(true);
+
+        if(surfaceHolder == null) {
+            // Get surfaceHolder object.
+            surfaceHolder = getHolder();
+            // Add this as surfaceHolder callback object.
+            surfaceHolder.addCallback(this);
+        }
+
+
+
+        // Set the parent view background color. This can not set surfaceview background color.
+        this.setBackgroundColor(Color.CYAN);
+
+        // Set current surfaceview at top of the view tree.
+        this.setZOrderOnTop(true);
+
+        this.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+    }
+
+
+    /* This method will be invoked to draw a circle in canvas. */
+    public void drawRedBall(float circleX, float circleY)
+    {
+        // Get and lock canvas object from surfaceHolder.
+        Canvas canvas = surfaceHolder.lockCanvas();
+
+
+        // Draw the surfaceview background color.
+        Paint surfaceBackground = new Paint();
+        surfaceBackground.setColor(Color.CYAN);
+        canvas.drawRect(0, 0, this.getWidth(), this.getHeight(), surfaceBackground);
+
+        // Draw the circle.
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        canvas.drawCircle(circleX, circleY, 100, paint);
+
+        // Unlock the canvas object and post the new draw.
+        surfaceHolder.unlockCanvasAndPost(canvas);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
