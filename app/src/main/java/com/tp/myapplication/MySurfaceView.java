8c0c94c76300aed032dab76d0e52eee2e06072d4
yoann2968 <yoann.spriet@gmail.com>
2018-10-09 19:17:27 +0200

    test

--- app/src/main/java/com/tp/myapplication/MySurfaceView.java
+++ app/src/main/java/com/tp/myapplication/MySurfaceView.java
@@ -19,6 +19,8 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
 
     private SurfaceHolder surfaceHolder = null;
 
+    private int test;
+
     public MySurfaceView(Context context) {
         super(context);
 
