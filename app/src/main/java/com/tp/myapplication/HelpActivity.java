510c32a4bebc3c5854dc3b99d046712e2eb7c97d
yoann2968 <yoann.spriet@gmail.com>
2018-10-16 13:26:30 +0200

    ajout de couleur et début activité d'aide

---
+++ app/src/main/java/com/tp/myapplication/HelpActivity.java
@@ -0,0 +1,39 @@
+package com.tp.myapplication;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+
+/**
+ * Created by yoann on 16/10/2018.
+ * classe permettant l'affichage de l'aide de l'application
+ */
+
+public class HelpActivity extends AppCompatActivity {
+
+    //Déclaration des differentes variables pour l'application
+    //Variable permettant de gérer l'affichage de l'aide
+    public TextView helpDisplay;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        //Initialisation des variables pour l'affichage de l'aide sur l'activité
+        helpDisplay = findViewById(R.id.help_display);
+
+        affichage();
+    }
+
+    //Méthode permettant de faire l'affichage de l'aide
+    private void affichage(){
+
+        helpDisplay.setText("Voici un prémice du vrai fichier d'aide qui viendra plus tard");
+
+    }
+
+}
