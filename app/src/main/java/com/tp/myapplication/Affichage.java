fa47f2ef9b5c1a9d4848d6092ae44d55aaebb441
damien97 <damienca97@gmail.com>
2018-10-16 02:03:12 -0800

    zone de texte, affichage capteur accelerometre

--- app/src/main/java/com/tp/myapplication/Affichage.java
+++ app/src/main/java/com/tp/myapplication/Affichage.java
@@ -2,25 +2,31 @@ package com.tp.myapplication;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.List;
 
-public class Affichage extends AppCompatActivity {
+public class Affichage extends AppCompatActivity implements SensorEventListener {
 
     final String TAG = "sensor";
 
     SensorManager mSensorManager;
     private Sensor mAccelerometer;
+    private TextView capteur1Valeur;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.affichage);
+        capteur1Valeur= (TextView) findViewById(R.id.capteur1);
 
         sensorDetection();
 
@@ -48,6 +54,40 @@ public class Affichage extends AppCompatActivity {
                 Log.v(TAG, "info:Accelerometer_not_found");
             }
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mSensorManager != null) {
+            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mSensorManager != null) {
+            mSensorManager.unregisterListener(this);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        // Many sensors return 3 values , one for each axis .
+        float Ax = sensorEvent.values[0];
+        float Ay = sensorEvent.values[1];
+        float Az = sensorEvent.values[2];
+
+
+        // Do something with this sensor value .
+        capteur1Valeur.setText(" TimeAcc = " + sensorEvent.timestamp + " Ax = " + Ax + " " + " Ay = " + Ay + " " + " Az = " + Az);
+        Log.v(TAG, " TimeAcc = " + sensorEvent.timestamp + " Ax = " + Ax + " " + " Ay = " + Ay + " " + " Az = " + Az);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 
 }
